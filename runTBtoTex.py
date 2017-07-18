f97c1174fede1c85e1e82858a2d98e4a170d9225
whonore <wolfhonore@gmail.com>
2017-07-18 09:43:59 -0400

    partially pep8-ify code

--- runTBtoTex.py
+++ runTBtoTex.py
@@ -2,12 +2,15 @@ import argparse
 import subprocess
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Translate a TB file into a LaTeX dictionary')
-	parser.add_argument('tbfile', metavar='F', type=str,
-		help='the Toolbox file to process')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Translate a TB file into a LaTeX dictionary')
+    parser.add_argument('tbfile',
+                        metavar='F',
+                        type=str,
+                        help='the Toolbox file to process')
+    args = parser.parse_args()
 
-	subprocess.call('python tbToTex.py ' + args.tbfile, shell=True)
-	subprocess.call('xelatex main.tex', shell=True)
+    subprocess.call('python tbToTex.py ' + args.tbfile, shell=True)
+    subprocess.call('xelatex main.tex', shell=True)
 
-	print("Success!")
+    print("Success!")
